50eb1d147a18eaa25e7f2dc808eaf6663c40280e
Scriptmad <69113016+Scriptmad@users.noreply.github.com>
2020-12-07 16:36:46 +0530

    Create NgServer.sh

---
+++ NgServer.sh
@@ -0,0 +1,58 @@
+figlet -f big SakidasoServer
+figlet -f small Dev by Scriptmad
+if [[ -f hoster2.sh ]]; then
+chmod +x ngrok
+sleep 2
+echo 'ngrok server'
+echo 'Please enable hotspot'
+sleep 2
+clear
+echo 'E'
+sleep 0.9
+echo 'N'
+sleep 0.9
+echo 'A'
+sleep 0.9
+echo 'B'
+sleep 0.9
+echo 'L'
+sleep 0.9
+echo 'E'
+sleep 0.9
+echo ''
+echo 'H'
+sleep 0.9
+echo 'O'
+sleep 0.9
+echo 'T'
+sleep 0.9
+echo 'S'
+sleep 0.9
+echo 'P'
+sleep 0.9
+echo 'O'
+sleep 0.9
+echo 'T'
+sleep 0.9
+echo ''
+sleep 2
+clear
+#Copy with Credit @Scriptmad and Sakidaso Ether Groups
+#Don't Change Any codes this will damage whole script
+echo 'Domain xyz.ngrok.io'
+else
+if [[ -f wget ]];then
+wget "https://raw.githubusercontent.com/Scriptmad/Hoster/main/Hoster.sh" -O Hoster.sh
+bash Hoster.sh
+echo 'Hoster.sh Downloaded'
+echo 'Now again open NgServer.sh on new terminal'
+else
+echo 'wget not found'
+pkg install wget
+fi
+fi
+./ngrok http 8080
+clear
+echo 'Join us'
+echo 'on Telegram'
+figlet -f small @Scriptmad
